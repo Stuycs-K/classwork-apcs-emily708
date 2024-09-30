ab9a649d5ba9e347aa11ffdccfdcdcab8516a868
Emily Mai <emilym384@nycstudents.net>
2024-09-29 20:22:31 -0400

    Completed test cases for returnCopy.

--- 2024-09-27/MyArrays.java
+++ 2024-09-27/MyArrays.java
@@ -4,7 +4,7 @@ public class MyArrays {
     for (int i = 0; i < nums.length; i++) {
       x += nums[i];
       if (i < nums.length - 1) {
-        ans += " ,";
+        x += ", ";
       }
     }
     return x + "]";
@@ -13,8 +13,13 @@ public class MyArrays {
   public static void main(String args[]) {
     //test cases for returnCopy
     int[] ary1 = new int[]{1,5,8};
-    int[] ary2 = returnCopy(int[] ary1);
-    System.out.println("Addresses are not the same:" ary1 == ary2 "and " );
-
+    int[] ary2 = returnCopy(ary1);
+    System.out.println("Expected addresses to be different: True vs my function: " + ary1 != ary2 + ". Expected elements to be the same: True vs my function: " + arrayToString(ary1).equals(arrayToString(ary2)));
+    ary1 = new int[]{7,1,5,0};
+    System.out.println("Expected addresses to be different: True vs my function: " + ary1 != ary2 + ". Expected elements to be the same: True vs my function: " + arrayToString(ary1).equals(arrayToString(ary2)));
+    ary1 = new int[]{1,2,3,4,5};
+    System.out.println("Expected addresses to be different: True vs my function: " + ary1 != ary2 + ". Expected elements to be the same: True vs my function: " + arrayToString(ary1).equals(arrayToString(ary2)));
+    ary1 = new int[]{8,2,6};
+    System.out.println("Expected addresses to be different: True vs my function: " + ary1 != ary2 + ". Expected elements to be the same: True vs my function: " + arrayToString(ary1).equals(arrayToString(ary2)));
   }
 }
