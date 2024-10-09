2b9eb43db7b3f4f5454297c9b282b6d319366ac4
Emily Mai <emilym384@nycstudents.net>
2024-10-08 23:39:37 -0400

    Copied over arr2DSum, replaceNegative, copy, and swapRC methods.

--- 10-07-2DArrayMethods/ArrayDemo.java
+++ 10-07-2DArrayMethods/ArrayDemo.java
@@ -78,7 +78,13 @@ public class ArrayDemo{
   /*Return the sum of all of the values in the 2D array
    *Use a nested loop instead of a helper method*/
   public static int arr2DSum(int[][]nums){
-    return 0;
+    int sum = 0;
+    for (int i = 0; i < nums.length; i++) {
+      for (int j = 0; j < nums[i].length; j++) {
+        sum += nums[i][j];
+      }
+    }
+    return sum;
   }
 
   //3. Modify a given 2D array of integer as follows:
@@ -87,7 +93,18 @@ public class ArrayDemo{
   //that negative with the value 1
   //-All other negatives replace with 0
   public static void replaceNegative(int[][] vals){
-
+    for (int i = 0; i < vals.length; i++) {
+      for (int j = 0; j < vals[i].length; j++) {
+        if (vals[i][j] < 0) {
+          if (i == j) {
+            vals[i][j] = 1;
+          }
+          else {
+            vals[i][j] = 0;
+          }
+        }
+      }
+    }
   }
 
   //4. Make a copy of the given 2d array.
@@ -95,15 +112,33 @@ public class ArrayDemo{
   //DO NOT use any built in methods that "copy" an array.
   //You SHOULD write a helper method for this.
   //If you don't see a good way to do that, you should stop and look at prior methods.
+  public static int[] returnCopy(int[] ary) {
+    int[] copy = new int[ary.length];
+    for (int i = 0; i < ary.length; i++) {
+      copy[i] = ary[i];
+    }
+    return copy;
+  }
+
   public static int[][] copy(int[][] nums){
-    return new int[1][1];
+    int[][] copy = new int[nums.length][];
+    for (int i = 0; i < nums.length; i++){
+      copy[i] = returnCopy(nums[i]);
+    }
+    return copy;
   }
 
   //5. Rotate an array by returning a new array with the rows and columns swapped.
   //   You may assume the array is rectangular and neither rows nor cols is 0.
   //   e.g. swapRC({{1,2,3},{4,5,6}}) returns {{1,4},{2,5},{3,6}}
   public static int[][] swapRC(int[][]nums){
-    return new int[1][1];
+    int[][] swappedAry = new int[nums[0].length][nums.length];
+    for (int i = 0; i < nums.length; i++) {
+      for (int j = 0; j < nums[0].length; j++) {
+        swappedAry[j][i] = nums[i][j];
+      }
+    }
+    return swappedAry;
   }
 
   //6. Make an HTML table by putting a table tag around the entire 2d array,
