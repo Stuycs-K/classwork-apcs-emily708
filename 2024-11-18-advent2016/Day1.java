ce55e43284193a89f2f10bcb21ea5f6da143810e
Emily Mai <emilym384@nycstudents.net>
2024-11-19 10:15:20 -0500

    Edited Day1 code.

--- 2024-11-18-advent2016/Day1.java
+++ 2024-11-18-advent2016/Day1.java
@@ -1,19 +1,40 @@
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
-
+import java.util.Arrays;
 public class Day1 {
+  public static String[] parse(String file) {
+    try {
+      Scanner input = new Scanner(new File(file));
+      String line = input.nextLine();
+      String[] data = line.split(", ");
+      //System.out.println(Arrays.toString(data));
+      return data;
+    } catch (FileNotFoundException e) {
+      System.out.println("File not found");
+      return null;
+    }
+  }
+
+  public static int solve(String[] data) {
+    int horizontalBlocks = 0;
+    int verticalBlocks = 0;
+    int degrees = 0;
+    for (int i = 0; i < data.length; i++) {
+      
+    }
+  }
   public static int countBlocks(String filename) {
     try {
       File file = new File(filename);
       Scanner input = new Scanner(file);
       int horizontalBlocks = 0;
       int verticalBlocks = 0;
-      int degrees = 90;
+      int degrees = 0;
       while (input.hasNext()) {
         String nextMove = input.next();
         String direction = nextMove.substring(0,1);
-        int blocks = Integer.parseInt(nextMove.substring(1,2));
+        int blocks = Integer.parseInt(nextMove.substring(1));
         if (direction.equals("L")) {
           degrees += 90;
         }
@@ -46,6 +67,6 @@ public class Day1 {
   }
 
     public static void main(String[] args) {
-    System.out.println(countBlocks("Day1.txt"));
+      System.out.println(parse("Day1.txt"));
   }
 }
