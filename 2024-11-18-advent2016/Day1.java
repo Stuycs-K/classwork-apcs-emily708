31b7dace17b10e7cd6afb3f9409d7ba1ca412ff1
Emily Mai <emilym384@nycstudents.net>
2024-11-24 21:10:41 -0500

    Completed part 2 of day 1.

--- 2024-11-18-advent2016/Day1.java
+++ 2024-11-18-advent2016/Day1.java
@@ -2,6 +2,7 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
 import java.util.Arrays;
+import java.util.ArrayList;
 
 public class Day1 {
   public static String[] parse(String file) {
@@ -46,8 +47,45 @@ public class Day1 {
     return Math.abs(horizontalBlocks) + Math.abs(verticalBlocks);
   }
    
+  public static int solve2(String[] data) {
+    int horizontalBlocks = 0;
+    int verticalBlocks = 0;
+    int degrees = 0;
+    ArrayList<String> visited = new ArrayList<String>();
+    for (int i = 0; i < data.length; i++) {
+      int blocks = Integer.parseInt(data[i].substring(1));
+      if (data[i].charAt(0) == 'L') {
+        degrees -= 1;
+      }
+      if (data[i].charAt(0) == 'R') {
+        degrees += 1;
+      }
+      for (int j = 0; j < blocks; j++) {
+        if (degrees % 4 == 0) {
+          verticalBlocks += 1;
+        }
+        if (degrees % 4 == 1) {
+          horizontalBlocks += 1;
+        }
+        if (degrees % 4 == 2) {
+          verticalBlocks -= 1;
+        }
+        if (degrees % 4 == 3) {
+          horizontalBlocks -= 1;
+        }
+        String coordinate = horizontalBlocks + ", " + verticalBlocks;
+        if (visited.contains(coordinate)) {
+          return Math.abs(horizontalBlocks) + Math.abs(verticalBlocks);
+        }
+        visited.add(coordinate);
+      }
+    }
+    return 0;
+  }
+
   public static void main(String[] args) {
     String[] data = parse("Day1.txt");
     System.out.println(solve(data));
+    System.out.println(solve2(data));
   }
 }
