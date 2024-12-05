ba2ad69fa2e9aff4b92661c59bb0d110fb39afb6
Emily Mai <emilym384@nycstudents.net>
2024-12-05 04:45:39 -0500

    Added integers and separator.

--- 2024-12-04-ColorScreen/Driver.java
+++ 2024-12-04-ColorScreen/Driver.java
@@ -13,8 +13,37 @@ public class Driver {
     }
   }
 
+  public static void integer() {
+    int[] integers = new int[3];
+    for (int i = 0; i < integers.length; i++) {
+      integers[i] = (int) (Math.random() * 100);
+    }
+    for (int i = 0; i < integers.length; i++) {
+      Text.go(2, (i * 20) + 20);
+      Text.color(Text.WHITE);
+      if (integers[i] < 25) {
+        Text.color(Text.RED + 60);
+      }
+      else if (integers[i] > 75) {
+        Text.color(Text.GREEN + 60);
+      }
+      System.out.print(integers[i]);
+    }
+  }
+
+  public static void separator() {
+    for (int i = 2; i < 80; i++) {
+      Text.go(3, i);
+      int color = (int) (Math.random() * 8) + 30;
+      Text.color(color);
+      System.out.print("-");
+    }
+  }
   public static void main(String[] args) {
     border();
+    integer();
+    separator();
     Text.go(31, 0);
+    System.out.print(Text.RESET);
   }
 }
