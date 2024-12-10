e6777002b0a068f37e7bb83f7a651ca4bc0aa6ad
Emily Mai <emilym384@nycstudents.net>
2024-12-10 06:05:28 -0500

    Added player's actions.

--- 2024-12-06-Adventurer/Game.java
+++ 2024-12-06-Adventurer/Game.java
@@ -5,11 +5,31 @@ public class Game {
     Adventurer p2 = new Ninja("Bob", 20);
 
     Scanner userInput = new Scanner(System.in);
-    String input = userInput.nextLine();
-    while (!input.equals("quit")) {
+    boolean continueGame = true;
+
+    while (continueGame) {
       System.out.println(p1 + ", " + p1.getHP() + "/" + p1.getmaxHP() + " HP, " + p1.getSpecial() + "/" + p1.getSpecialMax() + " " + p1.getSpecialName());
       System.out.println(p2 + ", " + p2.getHP() + "/" + p2.getmaxHP() + " HP, " + p2.getSpecial() + "/" + p2.getSpecialMax() + " " + p2.getSpecialName());
-  }
-
+      System.out.println("Type: (a)ttack / (sp)ecial / (su)pport / quit");
+      String input = userInput.nextLine();
+      if (input.equals("a") || input.equals("attack")) {
+        System.out.println(p1.attack(p2));
+      }
+      else if (input.equals("sp") || input.equals("special")) {
+        System.out.println(p1.specialAttack(p2));
+      }
+      else if (input.equals("su") || input.equals("support")) {
+        System.out.println(p1.support());
+      }
+      else if (input.equals("quit")) {
+        continueGame = false;
+        System.out.println("Ending game.");
+        break;
+      }
+      else {
+        System.out.println("Input was invalid. Please try again.");
+        continue;
+      }
+    }
   }
 }
