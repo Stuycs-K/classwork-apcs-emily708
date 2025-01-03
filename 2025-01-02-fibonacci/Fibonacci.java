489722742e0c6b072d4b898da42eaad1694a9716
Emily Mai <emilym384@nycstudents.net>
2025-01-03 10:15:35 -0500

    Worked on makeWords method.

--- 2025-01-02-fibonacci/Fibonacci.java
+++ 2025-01-02-fibonacci/Fibonacci.java
@@ -6,7 +6,19 @@ public class Fibonacci{
     return fib(n - 2) + fib(n - 1);
   }
 
+  public static void makeWords(int remainingLetters, String result, String alphabet){
+    if (remainingLetters == 0) {
+      System.out.println(result);
+    }
+    for (int i = 0; i < alphabet.length(); i++) {
+      String letter = alphabet.substring(i, i + 1);
+      remainingLetters -= 1;
+      result += letter;
+    }
+  }
+
   public static void main(String[] args) {
-    System.out.println(fib(46));
+    //System.out.println(fib(46));
+    makeWords(2,"","abc");
   }
 }
